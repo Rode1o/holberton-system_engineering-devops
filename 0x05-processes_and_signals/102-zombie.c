425ebbbf95fbf3ea5013ed504970c922bdd465ec
Rode1o <dumirola.dr@gmail.com>
2021-06-21 20:40:04 -0700

    betty

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -3,21 +3,33 @@
 #include <stdio.h>
 #include <sys/types.h>
 
+/**
+* infinite_while - for every zombie process created,
+* it displyas zombies process created, PID: ZOMBIE_PID
+* Return: zero
+*
+*/
+
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
+
+/**
+ * main - Create 5 zombie process
+ * Return: zero
+ */
 
 int main(void)
 {
 	int z;
 	pid_t pid;
 
-	for(z = 0; z < 5; z++)
+	for (z = 0; z < 5; z++)
 	{
 		pid = fork();
 		if (pid != 0)
